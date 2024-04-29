b3893b53b450e3946485584cc96fa14f112b9a9c
Diego Seminario <diego.seminario@globant.com>
2024-04-29 15:29:22 -0500

    Refresh neo tree after commit

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -7,6 +7,26 @@ return {
   },
   opts = {
     close_if_last_window = true,
+    autocmds = {
+      neotree_refresh = {
+        {
+          event = 'TermClose',
+          pattern = '*lazygit*',
+          desc = 'Refresh Neo-Tree sources when closing lazygit',
+          callback = function()
+            local manager_avail, manager = pcall(require, 'neo-tree.sources.manager')
+            if manager_avail then
+              for _, source in ipairs { 'filesystem', 'git_status', 'document_symbols' } do
+                local module = 'neo-tree.sources.' .. source
+                if package.loaded[module] then
+                  manager.refresh(require(module).name)
+                end
+              end
+            end
+          end,
+        },
+      },
+    },
     commands = {
       system_open = function(state)
         -- TODO: remove deprecated method check after dropping support for neovim v0.9
