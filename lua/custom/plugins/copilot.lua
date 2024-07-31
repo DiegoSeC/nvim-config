016f75e5fd2f99878fc28ede53bc56bbe1a2a487
Diego Seminario <diego.seminario@globant.com>
2024-07-31 12:18:21 -0500

    Adding copilot to statusline

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,16 +1,19 @@
 return {
-  'zbirenbaum/copilot.lua',
-  cmd = 'Copilot',
-  event = 'InsertEnter',
-  config = function()
-    require('copilot').setup {
-      suggestion = {
-        auto_trigger = true,
-        debounce = 150,
-        keymap = {
-          accept = false,
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup {
+        suggestion = {
+          auto_trigger = true,
+          debounce = 150,
+          keymap = {
+            accept = false,
+          },
         },
-      },
-    }
-  end,
+      }
+    end,
+  },
+  { 'AndreM222/copilot-lualine' },
 }
