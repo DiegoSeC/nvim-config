887dec1c83ef8adfc7bfc36fc5aa43878e79cda0
Diego Seminario <diego.seminario@globant.com>
2024-07-31 13:11:49 -0500

    Updating options

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -3,7 +3,22 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'typescript', 'css', 'c_sharp' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'typescript',
+        'css',
+        'c_sharp',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
