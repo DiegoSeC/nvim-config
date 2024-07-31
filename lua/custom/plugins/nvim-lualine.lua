c1fa804a3d899847446d11001a3ca5184e57b665
Diego Seminario <diego.seminario@globant.com>
2024-07-31 13:11:49 -0500

    Updating options

--- lua/custom/plugins/nvim-lualine.lua
+++ lua/custom/plugins/nvim-lualine.lua
@@ -1,11 +1,28 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  options = {
-    theme = 'wombat',
-    extensions = { 'neo-tree' },
-  },
-  config = function(_, opts)
-    require('lualine').setup(opts)
+  config = function()
+    require('lualine').setup {
+      options = {
+        theme = 'wombat',
+      },
+      extensions = { 'neo-tree' },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = {
+          'branch',
+          'diff',
+          {
+            'diagnostics',
+            sources = { 'nvim_diagnostic' },
+            symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
+          },
+        },
+        lualine_c = { 'filename' },
+        lualine_x = { 'copilot', 'encoding', 'fileformat', 'filetype' }, -- I added copilot here
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+    }
   end,
 }
--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -5,7 +5,22 @@ return {
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'typescript', 'css', 'c_sharp' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'typescript',
+        'css',
+        'c_sharp',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
