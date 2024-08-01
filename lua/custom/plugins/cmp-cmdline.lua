6375384eab8e92155a49d3abc1eddc5af0602a6c
Diego Seminario <diego.seminario@globant.com>
2024-08-01 16:21:50 -0500

    Adding new source to cmdline

--- lua/custom/plugins/cmp-cmdline.lua
+++ lua/custom/plugins/cmp-cmdline.lua
@@ -1,11 +1,13 @@
 return {
   'hrsh7th/cmp-cmdline',
+  dependencies = { 'hrsh7th/cmp-buffer' },
   config = function()
     local cmp = require 'cmp'
     cmp.setup.cmdline(':', {
       mapping = cmp.mapping.preset.cmdline(),
       sources = cmp.config.sources {
         { name = 'path' },
+        { name = 'buffer' },
       },
     })
   end,
