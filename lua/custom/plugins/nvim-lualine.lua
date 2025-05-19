c46ce620530d6b18c9ddc88e4d68ec48d76dab87
Diego Seminario <dseminario@are.com>
2025-05-19 11:47:34 -0500

    Adding Recording status to lualine

--- lua/custom/plugins/nvim-lualine.lua
+++ lua/custom/plugins/nvim-lualine.lua
@@ -1,3 +1,12 @@
+local function show_macro_recording()
+  local recording_register = vim.fn.reg_recording()
+  if recording_register == '' then
+    return ''
+  else
+    return '󰐾 Recording @' .. recording_register
+  end
+end
+
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
@@ -18,7 +27,13 @@ return {
             symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
           },
         },
-        lualine_c = { 'filename' },
+        lualine_c = {
+          'filename',
+          {
+            'macro-recording',
+            fmt = show_macro_recording,
+          },
+        },
         lualine_x = { 'encoding', 'fileformat', 'filetype' }, -- I added copilot here
         lualine_y = { 'copilot', 'progress' },
         lualine_z = { 'location' },
